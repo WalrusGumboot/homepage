a364889fa09da9258bf478a9474429874d3c4f13
Simeon Duwel <tameimpalafan123@gmail.com>
2023-09-08 15:17:00 +0200

    Add cargo search handler and Rust changes

--- src/lib/searchHandlers.ts
+++ src/lib/searchHandlers.ts
@@ -6,9 +6,10 @@ export interface SearchHandler {
 }
 
 export const searchHandlers: SearchHandler[] = [
-    { prefix: "rust", searchUrl: "https://doc.rust-lang.org/stable/std/index.html?search=" },
+    { prefix: "rs", searchUrl: "https://doc.rust-lang.org/stable/std/index.html?search=" },
     { prefix: "yt",   searchUrl: "https://www.youtube.com/results?search_query=" },
     { prefix: "aur",  searchUrl: "https://aur.archlinux.org/packages?O=0&K=" },
     { prefix: "arch", searchUrl: "https://archlinux.org/packages/?sort=&q=" },
-    { prefix: "py",   searchUrl: "https://docs.python.org/3/search.html?check_keywords=yes&area=default&q="}
+    { prefix: "py",   searchUrl: "https://docs.python.org/3/search.html?check_keywords=yes&area=default&q="},
+    { prefix: "cargo", searchUrl: "https://crates.io/search?q=" },
 ]
