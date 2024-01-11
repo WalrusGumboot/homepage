6a6bf76aa6dcd2f9ec38f2c842d55ee1cd0f06ef
Simeon Duwel <tameimpalafan123@gmail.com>
2024-01-11 11:16:25 +0100

    added typst symbol search handler

--- src/lib/searchHandlers.ts
+++ src/lib/searchHandlers.ts
@@ -12,4 +12,5 @@ export const searchHandlers: SearchHandler[] = [
     { prefix: "arch", searchUrl: "https://archlinux.org/packages/?sort=&q=" },
     { prefix: "py",   searchUrl: "https://docs.python.org/3/search.html?check_keywords=yes&area=default&q="},
     { prefix: "cargo", searchUrl: "https://crates.io/search?q=" },
+    { prefix: "tsym", searchUrl: "https://typst.app/docs/reference/symbols/sym/?query=" }
 ]
